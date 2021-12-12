a8c947142fe666d0eae4c737858741464d8c50bc
Perry Bunn <perry@auburn.edu>
2021-12-12 12:25:39 -0500

    :construction_worker: Updated pylint.yaml, requirments.txt, .gitignore, projcet file; Added pylint.yaml, pytest.yaml, pylintrc;

--- venv.sh
+++ venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # venv.sh
-# Installs Python 3.10.0 and creates a venv if there is not one already.
+# Installs Python 3.10.1 and creates a venv if there is not one already.
 #
 # TODO:
 # - [ ] add option for args to customize script (i.e. venv_name, python_ver, setup, packages)
@@ -8,42 +8,42 @@
 
 version=$(python3 -c 'import sys; print(sys.version_info[:])')
 version_att2=$(python3.10 -c 'import sys; print(sys.version_info[:])')
-pattern="(3, 10, 0, 'final', *)"
+pattern="(3, 10, 1, 'final', *)"
 
-if [[ $version =~ $pattern ]]
+if [[ $version =~ $pattern ]]```
 then
-	echo "Python 3.10.0 already installed."
+	echo "Python 3.10.1 already installed."
 else
 	if [[ $version_att2 =~ $pattern ]]
 	then
-		echo "Python 3.10.0 already installed."	
+		echo "Python 3.10.1 already installed."
 	else
-		if [[ ! -e "Python-3.10.0.tar.xz" ]]
+		if [[ ! -e "Python-3.10.1.tar.xz" ]]
 		then
-			wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
+			wget https://www.python.org/ftp/python/3.10.1/Python-3.10.1.tar.xz
 		else 
-			echo "'Python-3.10.0.tar.xz' already exist."
+			echo "'Python-3.10.1.tar.xz' already exist."
 		fi
 
 		{ # try
-			tar -xvf Python-3.10.0.tar.xz
+			tar -xvf Python-3.10.1.tar.xz
 		} || { #catch
 			echo "There was an error completing the unzip process. see above for an error."
 			exit 1
 		}
-		cd Python-3.10.0
+		cd Python-3.10.1
 		./configure --enable-optimizations
 		sudo make altinstall 
 		cd ..
 		echo "Install finished."
 		echo "Cleaning directory..."
-		rm -rf Python-3.10.0
+		rm -rf Python-3.10.1
 	fi
 fi
 
 if [[ ! -d "./venv" ]]
 then
-	echo "3.10.0 venv doesnt exist. Creating..."
+	echo "3.10.1 venv doesnt exist. Creating..."
 	python3.10 -m venv venv
 	echo "Done creating venv."
 	echo "Configuring venv..."
@@ -52,6 +52,6 @@ then
 	deactivate
 	echo "Done configuring venv."
 else
-	echo "3.10.0 venv already exist."
+	echo "3.10.1 venv already exist."
 fi
 echo "Run '$ source venv/bin/activate'"
